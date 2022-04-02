7fef25920c36fce44884acfff4e159bd0d7214fb
zsotroav <zsotroav@gmail.com>
2022-04-03 01:14:32 +0200

    minor redesign

--- PermuteMMO.WinFormsApp/MainForm.Designer.cs
+++ PermuteMMO.WinFormsApp/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxMMO = new System.Windows.Forms.CheckBox();
             this.panel5 = new System.Windows.Forms.Panel();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -41,18 +42,19 @@
             this.checkWantShiny = new System.Windows.Forms.CheckBox();
             this.buttonJSON = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.textBoxWarn = new System.Windows.Forms.TextBox();
             this.buttonPermutate = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.textBoxBonus = new System.Windows.Forms.TextBox();
-            this.textBoxBase = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.checkBoxAlpha = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxBase = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.numericSpawns2 = new System.Windows.Forms.NumericUpDown();
             this.comboBoxSpecies2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxLoc = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -60,9 +62,7 @@
             this.numericSpawns = new System.Windows.Forms.NumericUpDown();
             this.comboBoxSpecies = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBoxLoc = new System.Windows.Forms.ComboBox();
             this.textBoxSeed = new System.Windows.Forms.TextBox();
-            this.checkBoxMMO = new System.Windows.Forms.CheckBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panelFound = new System.Windows.Forms.Panel();
@@ -81,21 +81,30 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBoxMMO);
             this.panel1.Controls.Add(this.panel5);
             this.panel1.Controls.Add(this.buttonJSON);
             this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.textBoxWarn);
             this.panel1.Controls.Add(this.buttonPermutate);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.comboBoxLoc);
             this.panel1.Controls.Add(this.textBoxSeed);
-            this.panel1.Controls.Add(this.checkBoxMMO);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(644, 195);
             this.panel1.TabIndex = 0;
+            // 
+            // checkBoxMMO
+            // 
+            this.checkBoxMMO.AutoSize = true;
+            this.checkBoxMMO.Location = new System.Drawing.Point(6, 36);
+            this.checkBoxMMO.Name = "checkBoxMMO";
+            this.checkBoxMMO.Size = new System.Drawing.Size(226, 19);
+            this.checkBoxMMO.TabIndex = 0;
+            this.checkBoxMMO.Text = "Massive Mass Outbreak (MMO) mode";
+            this.checkBoxMMO.UseVisualStyleBackColor = true;
+            this.checkBoxMMO.CheckedChanged += new System.EventHandler(this.checkBoxMMO_CheckedChanged);
             // 
             // panel5
             // 
@@ -231,30 +240,21 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 6);
+            this.label7.Location = new System.Drawing.Point(6, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(35, 15);
             this.label7.TabIndex = 8;
             this.label7.Text = "Seed:";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 35);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(105, 15);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "Outbreak location:";
-            // 
             // textBoxWarn
             // 
             this.textBoxWarn.BackColor = System.Drawing.SystemColors.Control;
             this.textBoxWarn.ForeColor = System.Drawing.SystemColors.Control;
-            this.textBoxWarn.Location = new System.Drawing.Point(517, 3);
+            this.textBoxWarn.Location = new System.Drawing.Point(517, 6);
             this.textBoxWarn.Multiline = true;
             this.textBoxWarn.Name = "textBoxWarn";
             this.textBoxWarn.ReadOnly = true;
-            this.textBoxWarn.Size = new System.Drawing.Size(122, 89);
+            this.textBoxWarn.Size = new System.Drawing.Size(122, 86);
             this.textBoxWarn.TabIndex = 9;
             this.textBoxWarn.Text = "MMOs currently not fully operational in GUI mode. Use the website to get the spaw" +
     "n tables.";
@@ -274,71 +274,84 @@
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.Control;
             this.panel3.Controls.Add(this.textBoxBonus);
-            this.panel3.Controls.Add(this.textBoxBase);
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.checkBoxAlpha);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.textBoxBase);
             this.panel3.Controls.Add(this.textBox3);
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.numericSpawns2);
             this.panel3.Controls.Add(this.comboBoxSpecies2);
+            this.panel3.Controls.Add(this.comboBoxLoc);
             this.panel3.Controls.Add(this.label4);
-            this.panel3.Location = new System.Drawing.Point(260, 28);
+            this.panel3.Location = new System.Drawing.Point(260, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(251, 123);
+            this.panel3.Size = new System.Drawing.Size(251, 148);
             this.panel3.TabIndex = 5;
             // 
             // textBoxBonus
             // 
             this.textBoxBonus.Enabled = false;
-            this.textBoxBonus.Location = new System.Drawing.Point(3, 68);
+            this.textBoxBonus.Location = new System.Drawing.Point(3, 93);
             this.textBoxBonus.Name = "textBoxBonus";
             this.textBoxBonus.PlaceholderText = "Bonus Table";
             this.textBoxBonus.Size = new System.Drawing.Size(245, 23);
             this.textBoxBonus.TabIndex = 11;
             // 
-            // textBoxBase
-            // 
-            this.textBoxBase.Enabled = false;
-            this.textBoxBase.Location = new System.Drawing.Point(3, 39);
-            this.textBoxBase.Name = "textBoxBase";
-            this.textBoxBase.PlaceholderText = "Base Table";
-            this.textBoxBase.Size = new System.Drawing.Size(245, 23);
-            this.textBoxBase.TabIndex = 10;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 42);
+            this.label5.Location = new System.Drawing.Point(3, 72);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(74, 15);
             this.label5.TabIndex = 6;
             this.label5.Text = "Alphas only?";
+            this.label5.Visible = false;
             // 
             // checkBoxAlpha
             // 
             this.checkBoxAlpha.AutoSize = true;
             this.checkBoxAlpha.Enabled = false;
-            this.checkBoxAlpha.Location = new System.Drawing.Point(119, 43);
+            this.checkBoxAlpha.Location = new System.Drawing.Point(119, 73);
             this.checkBoxAlpha.Name = "checkBoxAlpha";
             this.checkBoxAlpha.Size = new System.Drawing.Size(15, 14);
             this.checkBoxAlpha.TabIndex = 5;
             this.checkBoxAlpha.UseVisualStyleBackColor = true;
+            this.checkBoxAlpha.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Outbreak location:";
+            // 
+            // textBoxBase
+            // 
+            this.textBoxBase.Enabled = false;
+            this.textBoxBase.Location = new System.Drawing.Point(3, 64);
+            this.textBoxBase.Name = "textBoxBase";
+            this.textBoxBase.PlaceholderText = "Base Table";
+            this.textBoxBase.Size = new System.Drawing.Size(245, 23);
+            this.textBoxBase.TabIndex = 10;
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.textBox3.Location = new System.Drawing.Point(3, 4);
+            this.textBox3.Location = new System.Drawing.Point(3, 3);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(245, 23);
             this.textBox3.TabIndex = 4;
-            this.textBox3.Text = "Second wave";
+            this.textBox3.Text = "MMO & second wave info";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 99);
+            this.label3.Location = new System.Drawing.Point(3, 124);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(110, 15);
             this.label3.TabIndex = 3;
@@ -347,7 +360,7 @@
             // numericSpawns2
             // 
             this.numericSpawns2.Enabled = false;
-            this.numericSpawns2.Location = new System.Drawing.Point(119, 97);
+            this.numericSpawns2.Location = new System.Drawing.Point(119, 122);
             this.numericSpawns2.Maximum = new decimal(new int[] {
             7,
             0,
@@ -361,19 +374,38 @@
             // 
             this.comboBoxSpecies2.Enabled = false;
             this.comboBoxSpecies2.FormattingEnabled = true;
-            this.comboBoxSpecies2.Location = new System.Drawing.Point(119, 68);
+            this.comboBoxSpecies2.Location = new System.Drawing.Point(119, 93);
             this.comboBoxSpecies2.Name = "comboBoxSpecies2";
             this.comboBoxSpecies2.Size = new System.Drawing.Size(129, 23);
             this.comboBoxSpecies2.TabIndex = 1;
+            this.comboBoxSpecies2.Visible = false;
+            // 
+            // comboBoxLoc
+            // 
+            this.comboBoxLoc.Enabled = false;
+            this.comboBoxLoc.FormattingEnabled = true;
+            this.comboBoxLoc.Items.AddRange(new object[] {
+            "Obsidian Fieldlands",
+            "Crimson Mirelands",
+            "Colbalt Coastlands",
+            "Coronet Highlands",
+            "Alabaster Icelands"});
+            this.comboBoxLoc.Location = new System.Drawing.Point(119, 35);
+            this.comboBoxLoc.Name = "comboBoxLoc";
+            this.comboBoxLoc.Size = new System.Drawing.Size(129, 23);
+            this.comboBoxLoc.TabIndex = 2;
+            this.comboBoxLoc.Text = "Obsidian Fieldlands";
+            this.comboBoxLoc.SelectedIndexChanged += new System.EventHandler(this.comboBoxLoc_SelectedIndexChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 71);
+            this.label4.Location = new System.Drawing.Point(3, 96);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 15);
             this.label4.TabIndex = 0;
             this.label4.Text = "Pokémon species:";
+            this.label4.Visible = false;
             // 
             // panel2
             // 
@@ -396,7 +428,7 @@
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(245, 23);
             this.textBox2.TabIndex = 4;
-            this.textBox2.Text = "First wave";
+            this.textBox2.Text = "Outbreak info";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label2
@@ -448,41 +480,13 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Pokémon species:";
             // 
-            // comboBoxLoc
-            // 
-            this.comboBoxLoc.Enabled = false;
-            this.comboBoxLoc.FormattingEnabled = true;
-            this.comboBoxLoc.Items.AddRange(new object[] {
-            "Obsidian Fieldlands",
-            "Crimson Mirelands",
-            "Colbalt Coastlands",
-            "Coronet Highlands",
-            "Alabaster Icelands"});
-            this.comboBoxLoc.Location = new System.Drawing.Point(122, 32);
-            this.comboBoxLoc.Name = "comboBoxLoc";
-            this.comboBoxLoc.Size = new System.Drawing.Size(129, 23);
-            this.comboBoxLoc.TabIndex = 2;
-            this.comboBoxLoc.Text = "Obsidian Fieldlands";
-            this.comboBoxLoc.SelectedIndexChanged += new System.EventHandler(this.comboBoxLoc_SelectedIndexChanged);
-            // 
             // textBoxSeed
             // 
-            this.textBoxSeed.Location = new System.Drawing.Point(47, 3);
+            this.textBoxSeed.Location = new System.Drawing.Point(50, 6);
             this.textBoxSeed.Name = "textBoxSeed";
             this.textBoxSeed.PlaceholderText = "16045686375800883966";
             this.textBoxSeed.Size = new System.Drawing.Size(204, 23);
             this.textBoxSeed.TabIndex = 1;
-            // 
-            // checkBoxMMO
-            // 
-            this.checkBoxMMO.AutoSize = true;
-            this.checkBoxMMO.Location = new System.Drawing.Point(272, 10);
-            this.checkBoxMMO.Name = "checkBoxMMO";
-            this.checkBoxMMO.Size = new System.Drawing.Size(226, 19);
-            this.checkBoxMMO.TabIndex = 0;
-            this.checkBoxMMO.Text = "Massive Mass Outbreak (MMO) mode";
-            this.checkBoxMMO.UseVisualStyleBackColor = true;
-            this.checkBoxMMO.CheckedChanged += new System.EventHandler(this.checkBoxMMO_CheckedChanged);
             // 
             // openFileDialog1
             // 
